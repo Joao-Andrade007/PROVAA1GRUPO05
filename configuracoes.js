1d6e671f9e6dd9325c780ddb476c3a33278a8cf4
Joao-Andrade007 <andrade4140@gmail.com>
2025-10-27 10:03:40 -0300

    Add files via upload

---
+++ configuracoes.js
@@ -0,0 +1,9 @@
+/**
+ * Arquivo de configuração de variáveis do projeto.
+ */
+
+// Requisito: cidadeImaginaria trocada por cidadeDosBolseiros
+const cidadeDoMapa = "cidadeDosBolseiros"; 
+const nivelZoom = 15;
+
+console.log(`Configuração carregada. Mapa focado em: ${cidadeDoMapa}`);
